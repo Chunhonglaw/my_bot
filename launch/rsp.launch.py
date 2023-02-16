9d560803a99db13c3fb1e53746ea4ffd963da45c
Chunhonglaw <chunhonglaw@hotmail.com>
2023-02-15 20:23:11 -0500

    update launch file

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -1,14 +1,11 @@
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch.substitutions import LaunchConfiguration
 from launch.actions import DeclareLaunchArgument
 from launch_ros.actions import Node
-
 import xacro
-
 
 def generate_launch_description():
 
@@ -22,13 +19,13 @@ def generate_launch_description():
     
     # Create a robot_state_publisher node
     params = {'robot_description': robot_description_config.toxml(), 'use_sim_time': use_sim_time}
+    
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
         parameters=[params]
     )
-
 
     # Launch!
     return LaunchDescription([
